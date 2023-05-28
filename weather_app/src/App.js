612bd08ab23484d9b3fe58ca828f453937435c65
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-05-28 06:43:07 +0530

    .

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
+const url = 'https://yahoo-weather5.p.rapidapi.com/weather';
 function App() {
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://yahoo-weather5.p.rapidapi.com/weather',
+      params: {
+        location: 'sunnyvale',
+        format: 'json',
+        u: 'f'
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': 'c908fc90d5msh8a4c92ce59a7eaep18880ejsnb9737895b1e3',
+        'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+      }
+    };
+    
+    try{
+      const res = await axios.request(options);
+      console.log(res.data)
+    }catch(err){
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
